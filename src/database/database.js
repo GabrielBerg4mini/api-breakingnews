4e4a109fbba46fb2cc2e4879ceca26994ddc01a0
GabrielBerg4mini <gabrielbergaminioficial@gmail.com>
2023-12-27 16:14:29 -0300

    :feat: adicionando o dotenv para a aplicação :docs: adicionando o .env e .env.example

--- src/database/database.js
+++ src/database/database.js
@@ -4,9 +4,7 @@ const connectDatabase = () => {
   console.log("Wait connecting to the database")
 
   mongoose
-    .connect(
-      "mongodb+srv://gabrielbergamini:licomemo12@cluster0.ms8ibdp.mongodb.net/?retryWrites=true&w=majority"
-    )
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB Atlas Connected"))
     .catch((error) => console.log(error))
 }
