c016d9c5f64cf3382d960e59bed0a708f0173647
GabrielBerg4mini <gabrielbergaminioficial@gmail.com>
2024-01-02 13:15:59 -0300

    entendo como funciona o JWT

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from "express"
 import connectDatabase from "./src/database/database.js"
-import userRoute from "./src/routes/user.route.js"
 import dotenv from "dotenv"
+
+import userRoute from "./src/routes/user.route.js"
+import authRoute from "./src/routes/auth.route.js"
+
 dotenv.config()
 
 const port = process.env.PORT || 3000
 const app = express()
+
 connectDatabase()
 app.use(express.json())
 app.use("/user", userRoute)
+app.use("/auth", authRoute)
 
 app.listen(port, async () => console.log(`Servidor rodando...`))
 
